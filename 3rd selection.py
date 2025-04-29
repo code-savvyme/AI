9acfcc994ea12301b9b0ec8d0e46716091250208
code-savvyme <snvisa347@gmail.com>
2025-04-29 06:49:36 +0530

    Delete 3rd selection.py

--- 3rd selection.py
+++
@@ -1,8 +0,0 @@
-def selectionSort(arr):
-    for i in range(len(arr)):
-        for j in range(i + 1, len(arr)):
-            if arr[i] > arr[j]:
-                arr[i],arr[j] = arr[j], arr[i]
-    return arr
-    
-print(selectionSort([9,56,75,4,61,26]))
